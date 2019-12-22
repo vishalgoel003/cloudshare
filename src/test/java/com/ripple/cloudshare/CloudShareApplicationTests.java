85ae373ba46a25a6391245de1652bbc8b06cb02b
Vishal Goel <vishalgoel003@gmail.com>
2019-12-22 13:57:51 +0530

    Adds base files, closes #1

---
+++ src/test/java/com/ripple/cloudshare/CloudShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ripple.cloudshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
